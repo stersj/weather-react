e41486e1a2ce38b6ee41b7811da556f274f2a6b7
stersj <esthersyl@gmail.com>
2020-07-25 10:10:29 +0200

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -5,17 +5,22 @@ export default function Footer() {
   return (
     <div className="footer">
       <small class="text-muted">
-        <a href="https://github.com/stersj/weather-react">Open-source code</a>,
-        by
+        <a href="https://github.com/stersj/weather-react" target="blank">
+          Open-source code
+        </a>
+        , by
         <a
           href="https://www.linkedin.com/in/esther-sim-jensen-47bb3120/"
           class="profileLink"
+          target="blank"
         >
           {" "}
           Esther Sim
         </a>
         , Hosted on
-        <a href="https://eager-edison-a06de0.netlify.app/">, Netlify</a>
+        <a href="https://eager-edison-a06de0.netlify.app/" target="blank">
+          , Netlify
+        </a>
       </small>
     </div>
   );
