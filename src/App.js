ed50574958f15b8327ca85b9115f9571b5d18968
stersj <esthersyl@gmail.com>
2020-08-04 22:12:53 +0200

    Deleted and moved footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import Footer from "./footer";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <Weather />
-      <Footer />
+      <div className="footer">
+        <small class="text-muted">
+          <a
+            href="https://github.com/stersj/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          , by
+          <a
+            href="https://www.linkedin.com/in/esther-sim-jensen-47bb3120/"
+            class="profileLink"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Esther Sim
+          </a>
+          , Hosted on
+          <a
+            href="https://eager-edison-a06de0.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            , Netlify
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
